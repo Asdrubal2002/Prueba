46702305fae3a38a231f394a76e83fa6b0763526
Asdrúbal Rodríguez Morales <103593282+Asdrubal2002@users.noreply.github.com>
2025-01-03 13:29:00 -0500

    Update vista_personas.php

--- app/secciones/vista_personas.php
+++ app/secciones/vista_personas.php
@@ -1,6 +1,17 @@
 <?php include('../templates/header.php'); ?>
 <?php include('personas.php'); ?>
 
+<?php
+if (isset($_POST['accion']) && $_POST['accion'] == 'limpiar') {
+    $dni = '';
+    $nombre = '';
+    $fecha_nacimiento = '';
+    $direccion = '';
+    $telefono = '';
+    $horarioEscogido = '';
+    $persona = null;
+}
+?>
 
 <div class="col-md-5">
     <form action="" method="post">
@@ -40,32 +51,39 @@
 
                 <div class="mb-3">
                     <label for="listaHorarios" class="form-label">Horarios</label>
-                    <select class="form-control" name="horarioID" id="listaHorarios" required>
+                    <select class="form-control" name="horarioid" id="listaHorarios" required>
                         <?php foreach ($horarios as $horario) { ?>
-                        <option value="<?php echo $horario['horarioID']; ?>" <?php 
-                            // Si el horario actual coincide con el horario escogido, se marca como seleccionado
-                            if ($horario['horarioID'] == $horarioEscogido) {
+                        <option value="<?php echo $horario['horarioid']; ?>" <?php 
+                            if ($horario['horarioid'] == $horarioEscogido) {
                                 echo 'selected';
                             }
                                 ?>>
-                            <?php echo $horario['horarioID']; ?> - <?php echo htmlspecialchars($horario['horario']); ?>
+                            <?php echo $horario['horarioid']; ?> - <?php echo htmlspecialchars($horario['horario']); ?>
                         </option>
                         <?php } ?>
                     </select>
                 </div>
 
                 <div class="btn-group" role="group" aria-label="Button group name">
-                <input type="hidden" name="personaID" value="<?php echo $persona['personaID']; ?>">
+                    <?php if ($persona != null) { ?>
+                    <input type="hidden" name="personaid" value="<?php echo $persona['personaid']; ?>">
+                    <?php } ?>
                     <button type="submit" name="accion" value="agregar" class="btn btn-success">
                         Añadir
                     </button>
-                    <button type="submit" name="accion" value="editar" class="btn btn-warning">
+                    <button type="submit" name="accion" value="editar" class="btn btn-warning"
+                        <?php echo ($persona == null ? 'disabled' : ''); ?>>
                         Editar
                     </button>
-                    <button type="submit" name="accion" value="borrar" class="btn btn-danger">
+                    <button type="submit" name="accion" value="borrar" class="btn btn-danger"
+                        <?php echo ($persona == null ? 'disabled' : ''); ?>>
                         Eliminar
                     </button>
+                    <button type="submit" name="accion" value="limpiar" class="btn btn-info">
+                        Limpiar formulario
+                    </button>
                 </div>
+
             </div>
         </div>
 
@@ -73,7 +91,11 @@
 </div>
 <div class="col-7">
     <div class="table-responsive">
-        <table class="table">
+        
+        <input type="text" id="filtroBusqueda" class="form-control" placeholder="Buscar por nombre o identificación"
+            onkeyup="filtrarTabla()">
+            <br>
+        <table class="table" id="tablaPersonas">
             <thead>
                 <tr>
                     <th scope="col">Identificación</th>
@@ -90,30 +112,48 @@
 
                         <?php foreach($persona["horarios"] as $horario){ ?>
                         <br>
-                        - <strong><?php echo $horario['horario']; ?></strong>
-
-                        <?php    } ?>
-
+                        <strong> Censo - <?php echo $horario['horario']; ?></strong>
+                        <?php } ?>
                     </td>
                     <td>
-
                         <form action="" method="post">
-                            <input type="hidden" name="personaID" id="personaID"
-                                value="<?php echo $persona['personaID']; ?>">
+                            <input type="hidden" name="personaid" id="personaid"
+                                value="<?php echo $persona['personaid']; ?>">
                             <input type="submit" value="Seleccionar" name="accion" class="btn btn-info">
                         </form>
-
-
-
                     </td>
                 </tr>
                 <?php endforeach; ?>
-
             </tbody>
         </table>
     </div>
-
 </div>
 
-
 <?php include('../templates/footer.php'); ?>
+
+<script>
+function filtrarTabla() {
+    var input, filtro, tabla, tr, td, i, txtValue;
+    input = document.getElementById("filtroBusqueda");
+    filtro = input.value.toUpperCase();
+    tabla = document.getElementById("tablaPersonas");
+    tr = tabla.getElementsByTagName("tr");
+
+    for (i = 0; i < tr.length; i++) {
+        td = tr[i].getElementsByTagName("td");
+        if (td.length > 0) {
+            txtValue = td[0].textContent || td[0].innerText; 
+            if (txtValue.toUpperCase().indexOf(filtro) > -1) {
+                tr[i].style.display = "";
+            } else {
+                txtValue = td[1].textContent || td[1].innerText; 
+                if (txtValue.toUpperCase().indexOf(filtro) > -1) {
+                    tr[i].style.display = "";
+                } else {
+                    tr[i].style.display = "none";
+                }
+            }
+        }
+    }
+}
+</script>
